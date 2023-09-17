0932c668fc971dc0257bc88eda3a7261fee449c5
dimitriaatos <d.a.ellinas@gmail.com>
2023-09-18 00:55:55 +0300

    feat: cms api intergration

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-// import Image from 'next/image'
-// import styles from './page.module.css'
+import Image from 'next/image'
+import styles from './page.module.css'
+import { api } from '@/common/helpers'
 
-export default function Home() {
-  // return (
-  //   <Image
-  //     src={}
-  //     alt="photos of projects"
-  //     sizes="100vw"
-  //     width={700}
-  //     height={475}
-  //     className={styles.image}
-  //   />
-  // )
+export default async function Home() {
+
+	const data = await api.getHome()
+
+	console.log(data.attributes.image)
+  return (
+    <Image
+      src={data.attributes.image}
+      alt="photos of projects"
+      sizes="100vw"
+      width={700}
+      height={475}
+      className={styles.image}
+    />
+  )
 }
--- common/helpers.ts
+++ common/helpers.ts
@@ -1,3 +1,20 @@
 export const capitalizedFirstLetter = (word: string): string =>
-	word.charAt(0).toUpperCase()
-	+ word.slice(1)
+  word.charAt(0).toUpperCase() + word.slice(1)
+
+const headers = new Headers({
+  Authorization: 'Bearer ' + process.env.STRAPI_API_TOKEN,
+})
+
+export const getData = async (route: string) => {
+  const res = await fetch(process.env.STRAPI_API_URL + route, {
+    headers,
+  })
+  const { data } = await res.json()
+  return data
+}
+
+export const api = Object.fromEntries(
+  ['home', 'about'].map((route) => {
+    return [`get${capitalizedFirstLetter(route)}`, () => getData('/' + route)]
+  })
+)
